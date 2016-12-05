0f864b1d935faa5056613689b4f6657fb776b8fc
Julian Barragan <julbarg@gmail.com>
2016-12-04 20:45:01 -0500

    Chapter 5

--- src/exam/Polimorfismo.java
+++ src/exam/Polimorfismo.java
@@ -1,7 +1,16 @@
 
 package exam;
 
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 public class Polimorfismo {
+	
+	final static int case10 = 50;
+	{
+		int o = 85;
+	}
 	public static void main(String[] args) {
 		
 		int i = 0564;
@@ -52,7 +61,118 @@ public class Polimorfismo {
 		lizard.getSomething();
 		
 		String[] j = new String[20];
-		j.length
+		StringBuffer buffer = new StringBuffer("012345");
+		buffer.replace(0, 1, "HERE");
+		System.out.println(buffer);
+		ArrayList<String> listArray = new ArrayList<>();
+		ListIterator<String> iterator = listArray.listIterator();
+		while(iterator.hasNext()){
+			System.out.println(iterator.next());
+		}
+		
+		ArrayList<Integer> listInt = new ArrayList<>();
+		listInt.add(0);
+		listInt.add(1);
+		listInt.add(2);
+		
+		ArrayList<Integer> listInt2 = new ArrayList<>();
+		listInt2.add(3);
+		listInt2.add(4);
+		listInt2.add(5);
+		
+		listInt.addAll(listInt2);
+		for(Integer l: listInt){
+			System.out.println(l);
+		}
+		
+		System.out.println();
+		
+		listInt2.set(0, 99);
+		listInt2.set(1, 98);
+		listInt2.set(2, 97);
+		
+		for(Integer l: listInt){
+			System.out.println(l);
+		}
+		
+		System.out.println();
+		
+		for(Integer l: listInt2){
+			System.out.println(l);
+		}
+		
+		Equals e1 = new Equals();
+		Equals e2 = new Equals();
+		System.out.println(e1.equals(e2));
+		System.out.println(e1.equals(e2));
+		System.out.println("12345".charAt(1));
+		"".indexOf("");
+		Period period = Period.ofDays(90);
+		char c = 'a';
+		int gqq = c;
+		System.out.println(gqq);
+		int[] ai = new int[30];
+		char[] k = {'1'};
+		StringBuffer y = new StringBuffer();
+		ArrayList<String> qa = new ArrayList<>(89);
+		String st = new String("1");
+		qa.add(st);
+		System.out.println(qa.contains(new String("1")));
+		System.out.println(qa);
+		
+		ArrayList<StringBuffer> listS = new ArrayList<>();
+		StringBuffer s1 = new StringBuffer("J");
+		listS.add(s1);
+		System.out.println(listS.contains(new StringBuffer("J")));
+		System.out.println(listS.contains("J"));
+		System.out.println(listS.contains(s1));
+		
+		boolean t = false;
+		if(t = true){
+			System.out.println(true);
+		}
+		
+		int count = 0;
+		for(i=0; i<10; i++){
+			count++;
+		}
+		
+		System.out.println();
+		System.out.println(count);
+		
+		count = 0;
+		for(i=0; i<10; ++i, System.out.println("terminal")){
+			count++;
+			System.out.println("inside for");
+		}
+		
+		System.out.println();
+		System.out.println(count);
+
+		System.out.println();
+		
+		int num2 = 10+10;
+		switch (num2) {
+		default:
+			System.out.println("default");
+
+		case 50:
+			for(String rrr : listArray){
+				
+			}
+			break;
+		}
+	}
+}
+
+class Equals {
+	public boolean equals(Equals obj) {
+		return true;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return false;
 	}
 }
 
@@ -78,6 +198,14 @@ class Reptile {
 	
 	public static void getSomething() {
 		System.out.println("Reptile");
+		
+		if(5>8){
+			System.out.println();
+		}
+		{
+			int y = 0;
+		}
+		
 	}
 }
 
